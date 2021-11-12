e2866534a68d1049f92179cc8fb29a597c7a09cf
parshya12345 <prashant25awchar@gmail.com>
2021-11-13 04:13:06 +0530

    push by prashant

---
+++ src/test/java/org/hexa/Employee.java
@@ -0,0 +1,17 @@
+package org.hexa;
+
+public class Employee {
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method3() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
